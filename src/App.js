71147195dfe5e951f800fb9b5c7ed3c2243e7eb3
Monu Simon M <monusimonm@Monus-MacBook-Air.local>
2024-03-09 10:21:24 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'; // Import any additional styles
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My React-Bootstrap App</h1>
-        <Button variant="primary">Click me</Button>
-      </header>
+      <Header/>
     </div>
   );
 }
