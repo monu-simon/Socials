9fd42b9a91d2f2d05df59b87caf342ac89756c05
Monu Simon M <monusimonm@Monus-MacBook-Air.local>
2024-03-09 10:55:51 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'; // Import any additional styles
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer className='footer bg-dark text-light text-center py-3'>
+        <Container>
+            <p>&copy;  2024 Socials - All rights reserved</p>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
